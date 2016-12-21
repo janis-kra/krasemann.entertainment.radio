90ce34ac45aa00b5e1dbb55598c50b82c24da33d
Janis Krasemann <janis.krasemann@enpit.de>
2016-12-22 00:53:35 +0100

    implement stopping running stations when another start request comes in

--- radio.js
+++ radio.js
@@ -4,18 +4,24 @@ require('dotenv').config();
 
 let logger;
 let vlc;
-
-const run = (url) => {
-  vlc = spawn(process.env.vlc, [url, '-I dummy']);
-  vlc.on('close', (code, signal) => {
-    logger.info(`received ${signal} - radio stopped`);
-  });
-};
+let running;
 
 const stop = () => {
+  running = false;
   if (vlc) {
     vlc.kill();
   }
+};
+
+const run = (url) => {
+  if (running) {
+    stop();
+  }
+  vlc = spawn(process.env.vlc, [url, '-I dummy']);
+  running = true;
+  vlc.on('close', (code, signal) => {
+    logger.info(`received ${signal} - radio stopped`);
+  });
 };
 
 const factory = (log) => {
